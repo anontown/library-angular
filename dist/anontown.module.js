8002056239d35abf702f17d32b86f9d715b117f1
kgtkr <kgtkr.jp+program@gmail.com>
2017-02-13 16:38:02 +0900

    tsc→ngc

--- dist/anontown.module.js
+++ dist/anontown.module.js
@@ -4,9 +4,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AtApiService, } from './api';
@@ -27,7 +24,6 @@ AnontownModule = __decorate([
             //サービス
             AtApiService
         ]
-    }),
-    __metadata("design:paramtypes", [])
+    })
 ], AnontownModule);
 export { AnontownModule };
